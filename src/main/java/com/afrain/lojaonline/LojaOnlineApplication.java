0406fc765b99149e89396259feb0059b935341aa
afrain <calixtoafrain@gmail.com>
2018-12-11 10:03:23 -0200

    Projeto criado

---
+++ src/main/java/com/afrain/lojaonline/LojaOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.afrain.lojaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaOnlineApplication.class, args);
+	}
+}
