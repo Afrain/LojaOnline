19b2adce4d658b545ab8b0ab217ffc8e576afb2f
afrain <calixtoafrain@gmail.com>
2018-12-13 07:37:00 -0200

    Criando operacao de estanciacao

--- src/main/java/com/afrain/lojaonline/LojaOnlineApplication.java
+++ src/main/java/com/afrain/lojaonline/LojaOnlineApplication.java
@@ -1,12 +1,32 @@
 package com.afrain.lojaonline;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.afrain.lojaonline.domain.Categoria;
+import com.afrain.lojaonline.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaOnlineApplication {
+public class LojaOnlineApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaOnlineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 }
