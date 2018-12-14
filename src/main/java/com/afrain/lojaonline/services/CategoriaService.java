b4d41dea0cdefcd73aeb6b63db4dd0d8cce6e7de
afrain <calixtoafrain@gmail.com>
2018-12-14 10:42:57 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/afrain/lojaonline/services/CategoriaService.java
+++ src/main/java/com/afrain/lojaonline/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.afrain.lojaonline.domain.Categoria;
 import com.afrain.lojaonline.repositories.CategoriaRepository;
+import com.afrain.lojaonline.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
