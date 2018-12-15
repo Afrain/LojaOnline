d45382ba06e36bae66fb37a5670347229cf2ed38
afrain <calixtoafrain@gmail.com>
2018-12-15 13:11:00 -0200

    EndPoint /clientes/{id} disponivel

---
+++ src/main/java/com/afrain/lojaonline/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.afrain.lojaonline.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.afrain.lojaonline.domain.Cliente;
+import com.afrain.lojaonline.repositories.ClienteRepository;
+import com.afrain.lojaonline.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
