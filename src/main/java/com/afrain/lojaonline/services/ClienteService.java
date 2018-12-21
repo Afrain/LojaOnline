4ac2164ddd72e95ebed67da0d54f78cf477a83e0
afrain <calixtoafrain@gmail.com>
2018-12-21 09:16:25 -0200

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/afrain/lojaonline/services/ClienteService.java
+++ src/main/java/com/afrain/lojaonline/services/ClienteService.java
@@ -60,7 +60,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma Cliente que possui entidades relacionadas!");
+			throw new DataIntegrityException("Não é possível excluir uma Cliente que possui Pedidos relacionados!");
 		}
 	}
 
